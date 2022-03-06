edfef175e0b72db26fced483fb7f7e08be782fa0
WMGameLive <andu890201@gmail.com>
2022-03-06 18:08:46 +0800

    update.

--- README.md
+++
@@ -1,5 +0,0 @@
-# AutoFishing
-A simple plugin that allow players to automatically fish.
-
-## Permission
-● autofishing.use - allow to automatically fish. (default: op)
--- src/main/java/wm/vdr/autofishing/Listeners.java
+++ src/main/java/wm/vdr/autofishing/Listeners.java
@@ -1,2 +1,50 @@
-package wm.vdr.autofishing;public class Listeners {
+package wm.vdr.autofishing;
+
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionHand;
+import org.bukkit.Material;
+import org.bukkit.craftbukkit.v1_18_R2.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerFishEvent;
+import org.bukkit.event.player.PlayerFishEvent.State;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class Listeners implements Listener {
+
+    private AutoFishing main = AutoFishing.instance;
+
+    @EventHandler
+    public void onFishing(PlayerFishEvent e) {
+        if(e.isCancelled()) return;
+
+        Player player = e.getPlayer();
+
+        if(!player.hasPermission("autofishing.use")) return;
+
+        if(e.getState() == State.BITE || e.getState() == State.CAUGHT_FISH) {
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    InteractionHand hand = getHand(player);
+                    if(hand == null) return;
+                    doRightClick(player, hand);
+                }
+            }.runTaskLater(main, e.getState() == State.BITE ? 5 : 20);
+            return;
+        }
+    }
+
+    private void doRightClick(Player player, InteractionHand hand) {
+        ServerPlayer serverPlayer = ((CraftPlayer) player).getHandle();
+        serverPlayer.gameMode.useItem(serverPlayer, serverPlayer.getLevel(), serverPlayer.getItemInHand(hand), hand);
+        serverPlayer.swing(hand, true);
+    }
+
+    private InteractionHand getHand(Player player) {
+        return player.getInventory().getItemInMainHand().getType().equals(Material.FISHING_ROD) ?
+                InteractionHand.MAIN_HAND : player.getInventory().getItemInOffHand().getType().equals(Material.FISHING_ROD) ?
+                InteractionHand.OFF_HAND : null;
+    }
 }
